419ecbfdb6c9cd6c5c77e44192b3ce2324cf8bf9
Ahmed Aladdin <ahmed.ibrahim035@eng-st.cu.edu.eg>
2023-12-01 08:40:26 +0200

    add a theme data for the app bar and colors

--- biscooter/lib/main.dart
+++ biscooter/lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/services.dart';
 import 'package:biscooter/screens/splash.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 }
 
 class MyApp extends StatelessWidget {
@@ -15,7 +17,17 @@ class MyApp extends StatelessWidget {
       title: 'Biscooter',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.white,
+          primary: const Color(0xFF2AF598),
+          secondary:const Color(0xFF009EFD),
+        ),
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontSize: 24,
+            color: Colors.black,
+          ),
+        ),
         useMaterial3: true,
       ),
       home: const Splash(),
