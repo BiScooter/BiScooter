8ec35bd4c0d79265d33167ae082e07b53294ac74
Peter Safwat <118059454+PeterSafwatHBK@users.noreply.github.com>
2023-12-24 09:27:20 +0200

    Update auth.controller.js

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,31 +1,92 @@
-// import express from "express";
-const express = require('express');
-const dotenv=require('dotenv');
-// import dotenv from "dotenv";
-const db = require('./Database/index');
-const authRouter = require ('./routes/auth.route.js');
-const globalErrorHandler = require('./controllers/ErrorController');
-// import authMiddleware from './middlewares/auth.middleware.js';
-// import userRouter from './routes/user.route.js';
-// import postRouter from "./routes/post.route.js";
-// import { isAdminMiddleware } from "./middlewares/permissions.middleware.js";
+const bcrypt = require("bcrypt");
+const User = require("../Models/FormatChecks.js");
+const AppError = require("./Error");
+const db = require("../Database/index");
+const catchAsync=require("./catchAsync.js");
+ exports.signup=catchAsync ( async (req, res, next) => {
+    // Validate the request body against the sign up schema
+    const {
+      FName,
+      MName,
+      LName,
+      Email,
+      Username,
+      Telephone,
+      Password,
+      ConfirmPassword,
+    } = req.body;
 
-dotenv.config();
-const app = express();
+    if (
+      !FName ||
+      !LName ||
+      !MName ||
+      !Email ||
+      !Password ||
+      !Username ||
+      !Telephone ||
+      !ConfirmPassword
+    ) {
+      return next(new AppError("Fill All Fields!", 409));
+    }
 
-const PORT = process.env.PORT || 8080;
+    if (!User.phoneCheck(Telephone)) {
+      return next(new AppError("Invalid Phone number!", 400));
+    }
+    if (!User.checkEmail(Email)) {
+      return next(new AppError("Email is invalid", 400));
+    }
+    const hashedPassword = await User.hashPassword(Password);
+    if (hashedPassword == -1)
+      return next(new AppError("Something is Wrong!!", 500));
+    const newUser =
+      await db.query(`INSERT INTO client Values(DEFAULT,1,'${Email}', '${Username}',
+        '${Telephone}','56789', '${FName}', '${MName}',' ${LName}', '${hashedPassword}','5',7,null);`);
+
+         res.status(200).send("successful");
+
+  });
+
+  exports.login=catchAsync (async (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    if (!email || !password) {
+      return next(new ErrorHandling("Please enter Email & password", 400));
+    }
+ 
+    if (!User.checkEmail(email)) {
+      return next(new ErrorHandling("Incorrect email or password", 401));
+    }
+  
+    const user = await db.query(
+      `SELECT password FROM client WHERE email = '${email}';`
+    );
+
+    const newUserId = await db.query(
+      `SELECT id FROM client WHERE email = '${email}';`
+    );
+    const truePassword = user["rows"][0]["password"] + "";
+   
+    const correct = await User.checkPassword(password, truePassword);
+    if (correct == -1)
+      return next(new ErrorHandling("some thing went wrong try again", 500));
+    
+    if (user["rowCount"] == 0 || !correct) {
+      return next(new ErrorHandling("incorrect email or password", 401));
+    }
+    res.status(200).send("Successfully Logged in");
+  });
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+  exports.me=catchAsync(async (req, res) => {
+    const email=req.body.email;
+    const result = await db.query(`SELECT * FROM client where Email='${email}';`);
+    res.json({
+      status: 200,
+      message: result.rows
+    });
+  });
 
-app.use('/auth', authRouter);
-// app.use('/users', authMiddleware, isAdminMiddleware, userRouter);
-// app.use('/posts', authMiddleware, postRouter);
 
 
-app.listen(PORT, (err) => {
-  if (err) return console.error(err);
-  console.log(`Server started listening at port ${PORT}`);
-});
-app.use(globalErrorHandler);
+
