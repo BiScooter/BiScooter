7952a2bc856e37b96b4e4a77dc238e46b76b610c
Ahmed Aladdin <ahmed.ibrahim035@eng-st.cu.edu.eg>
2023-12-28 22:34:49 +0200

    activate send a complaint

--- biscooter/lib/services/connection.dart
+++ biscooter/lib/services/connection.dart
@@ -1,7 +1,7 @@
 class Connection
 {
   // Define here the variables that you want to use as dimensions
-  static const String  _baseUrl = "http://192.168.42.199:8080";
+  static const String  _baseUrl = "http://192.168.1.12:8080";
 
   //initialize variables in here
   const Connection();
